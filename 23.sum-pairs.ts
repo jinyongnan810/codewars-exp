4982e5c026d16fbbccbc93c38e91f319a6423f38
Kin Yuunan <yuunan.kin@gmail.com>
2021-12-23 15:38:32 +0900

    try0

--- 23.sum-pairs.ts
+++ 23.sum-pairs.ts
@@ -1,20 +1,15 @@
 export function sumPairs(ints: number[], s: number): [number, number] | void {
-  let i = 0;
   let res: [number, number] | undefined = undefined;
-  while (true) {
-    for (; i < ints.length - 1; i++) {
-      const num = ints[i];
-      const rest = s - num;
-      const restIndex = ints.indexOf(rest, i + 1);
-      // if exists
-      if (restIndex > i) {
-        res = [num, rest];
-        ints = ints.slice(i, restIndex);
-        i = 0;
-        break;
-      }
+  let maxIndex = ints.length;
+  for (let i = 0; i < maxIndex - 1; i++) {
+    const num = ints[i];
+    const rest = s - num;
+    const restIndex = ints.indexOf(rest, i + 1);
+    // if exists
+    if (restIndex > i && restIndex < maxIndex) {
+      res = [num, rest];
+      maxIndex = restIndex;
     }
-    if (i >= ints.length - 1) break;
   }
   return res;
 }
