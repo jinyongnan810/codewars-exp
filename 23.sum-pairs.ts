4ffcf00107443626bc4eb8329659ea9610958045
Kin Yuunan <yuunan.kin@gmail.com>
2021-12-23 15:58:32 +0900

    fix bug

--- 23.sum-pairs.ts
+++ 23.sum-pairs.ts
@@ -1,19 +1,17 @@
 export function sumPairs(ints: number[], s: number): [number, number] | void {
   const findIndexBetween = (target: number, start: number, end: number) => {
-    for (let i = start; i <= end; i++) {
+    for (let i = start; i < end; i++) {
       if (ints[i] == target) return i;
     }
     return -1;
   };
   let res: [number, number] | undefined = undefined;
   let maxIndex = ints.length;
-  for (let i = 0; i < maxIndex - 1; i++) {
-    const num = ints[i];
-    const rest = s - num;
-    const restIndex = findIndexBetween(rest, i + 1, maxIndex - 1);
+  for (let i = 0; i < maxIndex; i++) {
+    const restIndex = findIndexBetween(s - ints[i], i + 1, maxIndex);
     // if exists
     if (restIndex != -1) {
-      res = [num, rest];
+      res = [ints[i], s - ints[i]];
       maxIndex = restIndex;
     }
   }
