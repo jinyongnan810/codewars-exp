550707142dea809ff21371396d3ed6ca2858a164
Kin Yuunan <yuunan.kin@gmail.com>
2021-12-28 14:55:58 +0900

    day 23(passed)

--- 28.next-big-number.ts
+++ 28.next-big-number.ts
@@ -2,12 +2,30 @@ export function nextBigger(n: number): number {
   const nStr = n.toString();
   // like 999
   if ((n + 1).toString().length > nStr.length) return -1;
-  const allPatterns: number[] = shuffle("", nStr).map((x) => Number(x));
-  const largerThanN = allPatterns.filter((x) => x > n);
-  const closest = largerThanN.sort()[0];
-  return closest ?? -1;
-}
+  // like 9876543210
+  if (
+    nStr
+      .split("")
+      .sort((a, b) => Number(b) - Number(a))
+      .join("") == nStr
+  )
+    return -1;
+  for (let i = nStr.length - 2; i >= 0; i--) {
+    const allPatterns: number[] = shuffle(nStr.slice(0, i), nStr.slice(i)).map(
+      (x) => Number(x)
+    );
+    const largerThanN = allPatterns.filter((x) => x > n);
+    if (largerThanN.length > 0) {
+      const closest = largerThanN.reduce(
+        (prev, cur) => (cur < prev ? cur : prev),
+        Number.MAX_SAFE_INTEGER
+      );
+      return closest;
+    }
+  }
 
+  return -1;
+}
 const shuffle = (first: string, rest: string): string[] => {
   if (rest.length === 1) return [first + rest];
   let res: string[] = [];
