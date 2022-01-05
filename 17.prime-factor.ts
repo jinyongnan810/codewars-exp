99289fb175c519447050fdc82bc9a8c53412e191
Kin Yuunan <yuunan.kin@gmail.com>
2022-01-05 14:30:22 +0900

    prime-factors other solution

--- 17.prime-factor.ts
+++ 17.prime-factor.ts
@@ -46,4 +46,29 @@ export class G964 {
         .join("");
     return res;
   }
+
+  public static primeFactors_others = (n: number) => {
+    var i = 2,
+      count,
+      output = "";
+    while (n > 1) {
+      count = 0;
+      while (n % i == 0) {
+        count++;
+        n /= i;
+      }
+      switch (count) {
+        case 0:
+          output += "";
+          break;
+        case 1:
+          output += "(" + i + ")";
+          break;
+        default:
+          output += "(" + i + "**" + count + ")";
+      }
+      i++;
+    }
+    return output;
+  };
 }
