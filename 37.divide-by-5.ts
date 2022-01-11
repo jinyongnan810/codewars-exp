8dedaf30c260c180df4592293f4feb89fe215a80
Kin Yuunan <yuunan.kin@gmail.com>
2022-01-12 08:42:58 +0900

    day33(partial success)

--- 37.divide-by-5.ts
+++ 37.divide-by-5.ts
@@ -1,7 +1,14 @@
 // https://www.codewars.com/kata/5647c3858d4acbbe550000ad/train/typescript
 // https://www.rapidtables.com/convert/number/binary-to-decimal.html
-export const divisibleByFive =
-  /^0*((1(10)*0(01*000)*1)|(111(101*0)*(01)*11))+0*$/;
+export const divisibleByFive = {
+  test: (x: string) => {
+    console.log(x);
+    return /^0*(((1(10)*0)|(1(10)*11))((01*01)*|((01*000)|(01*0011))*)1)+0*$/.test(
+      x
+    );
+  },
+};
+// not work with 10011010001001, need revision
 
 // http://ivanzuzak.info/noam/webapps/fsm2regex/
 // #states
@@ -29,7 +36,5 @@ export const divisibleByFive =
 // E:a>D
 // E:b>E
 
-// /0+1((1+(0+11)(01*01)*01*0)0)*(0+11)(01*01)*1+(0+1((1+(0+11)(01*01)*01*0)0)*(0+11)(01*01)*1)(0+1((1+(0+11)(01*01)*01*0)0)*(0+11)(01*01)*1)*(0+1((1+(0+11)(01*01)*01*0)0)*(0+11)(01*01)*1)/
-
-// covert an nfa to regex
+// ********covert an nfa to regex********
 // https://www.youtube.com/watch?v=UKYvP8aS7fM
