30f9845b070cf318ef55cbb2e53175128eaf4526
Kin Yuunan <yuunan.kin@gmail.com>
2022-01-20 08:49:43 +0900

    day41(create a huffman tree in code)

--- 42.huffman-encoding.ts
+++ 42.huffman-encoding.ts
@@ -8,6 +8,41 @@ function frequencies(s: string): [string, number][] {
   });
   return res;
 }
+
+class HuffmanNode {
+  value: number;
+  char?: string;
+  left: HuffmanNode | null;
+  right: HuffmanNode | null;
+  constructor(value: number, char?: string) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.char = char;
+  }
+}
+
+const createHuffmanTree = (freqs: [string, number][]) => {
+  // create single trees
+  let nodes = freqs.map((freq) => new HuffmanNode(freq[1], freq[0]));
+  // form a huffman tree
+  while (nodes.length !== 1) {
+    // pick 2 nodes with least freq
+    const first = nodes.reduce((a, b) => (a.value < b.value ? a : b), nodes[0]);
+    nodes = nodes.filter((node) => node != first);
+    const second = nodes.reduce(
+      (a, b) => (a.value < b.value ? a : b),
+      nodes[0]
+    );
+    nodes = nodes.filter((node) => node != second);
+    const newNode = new HuffmanNode(first.value + second.value);
+    newNode.left = first;
+    newNode.right = second;
+    nodes.push(newNode);
+  }
+  return nodes[0];
+};
+
 // build a huffman tree: https://www.geeksforgeeks.org/huffman-coding-greedy-algo-3/
 // takes: [ [String,Int] ], String; returns: String (with "0" and "1")
 function encode(freqs: [string, number][], s: string): string {}
