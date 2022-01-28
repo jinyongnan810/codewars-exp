c9f56cad812ee5902059c4416b3433f853e6b16a
Kin Yuunan <yuunan.kin@gmail.com>
2022-01-28 20:56:32 +0900

    day48(first worked one, but with memory issues)

--- 46.sliding-down-pyramids.ts
+++ 46.sliding-down-pyramids.ts
@@ -1,35 +1,31 @@
 // https://www.codewars.com/kata/551f23362ff852e2ab000037/train/javascript
 // https://projecteuler.net/problem=18
 
-const sumOfSubPyramid = (pyramid: number[][], row: number, pos: number) => {
-  let sum = pyramid[row][pos];
-  let lengthOfSubPyramidRow = 2;
-  for (let i = row + 1; i < pyramid.length; i++) {
-    for (let j = 0; j < lengthOfSubPyramidRow; j++) {
-      sum += pyramid[i][pos + j];
-    }
-    lengthOfSubPyramidRow++;
-  }
-  return sum;
+type PyrimidNode = {
+  value: number;
+  possibles: number[];
 };
 
 function longestSlideDown(pyramid: number[][]) {
-  let sum = pyramid[0][0];
-  let position = 0;
-  for (let i = 1; i < pyramid.length; i++) {
-    // slide down to nearest ones
-    const leftSum = sumOfSubPyramid(pyramid, i, position);
-    const rightSum = sumOfSubPyramid(pyramid, i, position + 1);
-    console.log(
-      `level ${i} position:${position}: left sum:${leftSum}, right sum:${rightSum}. Choose:${
-        leftSum > rightSum ? pyramid[i][position] : pyramid[i][position + 1]
-      } `
-    );
-    if (leftSum > rightSum) sum += pyramid[i][position];
-    else {
-      sum += pyramid[i][position + 1];
-      position++;
+  const pyrimidNodes: PyrimidNode[][] = pyramid.map((level) =>
+    level.map((node) => ({ value: node, possibles: [] }))
+  );
+  pyrimidNodes[0][0].possibles = [pyrimidNodes[0][0].value];
+  for (let i = 1; i < pyrimidNodes.length; i++) {
+    for (let j = 0; j < pyrimidNodes[i].length; j++) {
+      if (j != 0) {
+        pyrimidNodes[i - 1][j - 1].possibles.forEach((x) =>
+          pyrimidNodes[i][j].possibles.push(x + pyrimidNodes[i][j].value)
+        );
+      }
+      if (j != pyrimidNodes[i].length - 1)
+        pyrimidNodes[i - 1][j].possibles.forEach((x) =>
+          pyrimidNodes[i][j].possibles.push(x + pyrimidNodes[i][j].value)
+        );
     }
   }
-  return sum;
+  return pyrimidNodes[pyrimidNodes.length - 1].reduce((prev, cur) => {
+    const largest = cur.possibles.reduce((p, c) => (c > p ? c : p), 0);
+    return largest > prev ? largest : prev;
+  }, 0);
 }
