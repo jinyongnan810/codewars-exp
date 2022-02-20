59e28bbaaca7dba33cedf9d036071343ea0d6f74
Kin Yuunan <yuunan.kin@gmail.com>
2022-02-21 08:16:33 +0900

    almost

--- 60.n2-ends-with-n.ts
+++ 60.n2-ends-with-n.ts
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/584dee06fe9c9aef810001e8/train/javascript
 // "1","5","6","25","76","376","625","9376","90625","109376","890625","2890625","7109376"
-const greenCache: string[] = ["1", "5", "6"];
+const greenCache: bigint[] = [1n, 5n, 6n];
 // const greenCache: string[] = [
 //   "1",
 //   "5",
@@ -51,85 +51,44 @@ const greenCache: string[] = ["1", "5", "6"];
 //   "6619977392256259918212890625",
 // ];
 
-// @ts-ignore
-const multiply_60 = (x: string, y: string): string => {
-  let res = "0";
-  for (let i = y.length - 1; i >= 0; i--) {
-    const m = multiply1Digit_60(x, y[i], i);
-    const zeros = y.length - 1 - i;
-    res = sum_60(res, m + (zeros > 0 ? "0".repeat(zeros) : ""));
-  }
-  return res;
-};
-// @ts-ignore
-const multiply1Digit_60 = (x: string, y: string, neededDigits): string => {
-  const yNum = +y;
-  const stack: number[] = [];
-  let prevOverflow = 0;
-  for (let i = x.length - 1; i >= x.length - 1 - neededDigits; i--) {
-    const m = +x[i] * yNum + prevOverflow;
-    stack.push(m % 10);
-    prevOverflow = Math.floor(m / 10);
-  }
-  if (prevOverflow != 0) stack.push(prevOverflow);
-  return stack.reverse().join("");
-};
-// @ts-ignore
-const sum_60 = (a: string, b: string): string => {
-  if (a.length > b.length) b = b.padStart(a.length, "0");
-  else a = a.padStart(b.length, "0");
-  let plus1 = 0;
-  const stack = [];
-  for (let i = a.length - 1; i >= 0; i--) {
-    const sum = +a[i] + +b[i] + plus1;
-    if (sum > 9) plus1 = 1;
-    else plus1 = 0;
-    stack.push((sum % 10).toString());
-  }
-  if (plus1 == 1) stack.push("1");
-  return stack.reverse().join("");
-};
-
 function green(n: number): BigInt {
+  console.log(`n:${n}`);
   if (n <= greenCache.length) {
-    return BigInt(greenCache[n - 1]);
+    return greenCache[n - 1];
   }
   for (let i = greenCache.length; i < n; ) {
     const prev1 = greenCache[i - 1];
-    const ptn1 = prev1;
-    let ptn2 = "";
+    const ptn1: bigint = prev1;
+    let ptn2: bigint = 1n;
     // console.log(JSON.stringify(greenCache));
     for (let x = i - 2; x >= 1; x--) {
       // console.log(
       //   `prev1:${prev1},x:${x},i:${i},greenCache[x]:${greenCache[x]}`
       // );
-      if (!prev1.endsWith(greenCache[x])) {
+      if (!prev1.toString().endsWith(greenCache[x].toString())) {
         ptn2 = greenCache[x];
         break;
       }
     }
     // console.log(`prev1:${prev1},ptn2:${ptn2}`);
-    const l1 = ptn1.length;
-    // console.log(`i=${i}`);
+    const l1 = ptn1.toString().length;
     // console.log(`cache:${greenCache}`);
-    const hit: string[] = [];
+    const hit: bigint[] = [];
     for (let x = 1; x <= 9; x++) {
-      const nextTry = sum_60(ptn1, `${x}${"0".repeat(l1)}`);
-      const m = multiply_60(nextTry, nextTry);
-      // console.log(`trying2:${nextTry}`);
-      if (m.endsWith(nextTry)) {
-        // console.log(`pushed:${nextTry}`);
+      const zeros: bigint = BigInt(`${x}${"0".repeat(l1)}`);
+      const nextTry: bigint = ptn1 + zeros;
+      const m = (nextTry ** 2n).toString();
+      if (m.endsWith(nextTry.toString())) {
         hit.push(nextTry);
         i++;
         break;
       }
     }
     for (let x = 1; x <= 9; x++) {
-      const nextTry = sum_60(ptn2, `${x}${"0".repeat(l1)}`);
-      // console.log(`trying1:${nextTry}`);
-      const m = multiply_60(nextTry, nextTry);
-      if (m.endsWith(nextTry)) {
-        // console.log(`pushed:${nextTry}`);
+      const zeros: bigint = BigInt(`${x}${"0".repeat(l1)}`);
+      const nextTry: bigint = ptn2 + zeros;
+      const m = (nextTry ** 2n).toString();
+      if (m.endsWith(nextTry.toString())) {
         hit.push(nextTry);
         i++;
         break;
@@ -137,15 +96,9 @@ function green(n: number): BigInt {
     }
     hit
       .sort((a, b) => {
-        if (a.length > b.length) return 1;
-        else if (a.length < b.length) return -1;
-        else return BigInt(a) > BigInt(b) ? 1 : -1;
+        return a > b ? 1 : -1;
       })
       .forEach((x) => greenCache.push(x));
   }
   return BigInt(greenCache[n - 1]);
 }
-// 500: 6s, 1000:33s
-console.log(Date.now());
-console.log(green(1000));
-console.log(Date.now());
