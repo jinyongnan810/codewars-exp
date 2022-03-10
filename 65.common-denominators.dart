03db078f754db4a917173b318ba698fea98266e0
Kin Yuunan <yuunan.kin@gmail.com>
2022-03-11 08:23:15 +0900

    day78(try3,close)

--- 65.common-denominators.dart
+++ 65.common-denominators.dart
@@ -2,6 +2,31 @@
 import 'dart:math';
 
 List<int> primes = [2, 3];
+
+Map<int, int> primeDecomposition(int n) {
+  // get all the primes
+  if (n > primes.last) {
+    for (int i = primes.last + 1; i <= n; i++) {
+      checkIsPrime(i);
+    }
+  }
+  // get decompositions
+  int nTmp = n;
+  Map<int, int> res = {};
+  for (int i = 0; i < primes.length; i++) {
+    int prime = primes[i];
+    if (prime > nTmp) break;
+    while (nTmp % prime == 0) {
+      nTmp ~/= prime;
+      if (res[prime] != null)
+        res[prime] = (res[prime] ?? 0) + 1;
+      else
+        res[prime] = 1;
+    }
+  }
+  return res;
+}
+
 bool checkIsPrime(int n) {
   if (n < primes.last) {
     return primes.contains(n);
@@ -39,15 +64,29 @@ String convertFrac(List<List<int>> list) {
     return [a, b];
   }).toList();
   print(sList);
-  // get min common multiple
   List<int> bs = sList.map((e) => e[1]).toList();
-  int gcd = getGreatestCommonDivisor(bs);
-  bs = bs.map((e) => e ~/ gcd).toList();
-  final minCommonMultiple =
-      bs.fold<int>(1, (previousValue, element) => previousValue * element) *
-          gcd;
-  print(gcd);
-  print(bs);
+  // get prime compositions
+  final decompositions = bs.map((e) => primeDecomposition(e)).toList();
+  print(decompositions);
+  final maxPrimes =
+      decompositions.fold<Map<int, int>>({}, (previousValue, element) {
+    Map<int, int> res = {};
+    [...element.keys, ...previousValue.keys].forEach((key) {
+      if ((previousValue[key] ?? 0) < (element[key] ?? 0)) {
+        res[key] = element[key] ?? 0;
+      } else {
+        res[key] = previousValue[key] ?? 0;
+      }
+    });
+    return res;
+  });
+  print(maxPrimes);
+  // get min common multiple
+  final minCommonMultiple = maxPrimes.keys.fold<int>(
+      1,
+      (previousValue, key) =>
+          previousValue * (pow(key, (maxPrimes[key] ?? 0)).toInt()));
+  print(minCommonMultiple);
 
   // result
   return sList.map((l) {
