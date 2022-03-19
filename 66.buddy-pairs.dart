8052ab6243111b449b38a15b5e2c4736cc903b7d
Kin Yuunan <yuunan.kin@gmail.com>
2022-03-20 08:59:40 +0900

    tiny improvement

--- 66.buddy-pairs.dart
+++ 66.buddy-pairs.dart
@@ -1,4 +1,7 @@
 // https://www.codewars.com/kata/59ccf051dcc4050f7800008f/train/dart
+
+Map<int, int> sumOfDividersCache = {};
+
 List<int>? buddy(int start, int limit) {
   for (int i = start; i <= limit; i++) {
     int sum = sumOfDividers(i);
@@ -10,13 +13,19 @@ List<int>? buddy(int start, int limit) {
 }
 
 int sumOfDividers(int n) {
+  if (sumOfDividersCache.containsKey(n)) return sumOfDividersCache[n]!;
   int sum = 1;
   for (int i = 2; i < n; i++) {
     if (n % i == 0) sum += i;
   }
+  sumOfDividersCache[n] = sum;
   return sum;
 }
 
 main() {
   print(buddy(10, 50));
+  print(buddy(200, 1000));
+  print(buddy(57345, 90061));
+  print(buddy(57345, 90061));
+  print(sumOfDividersCache.keys.length);
 }
