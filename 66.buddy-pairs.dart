c9131104a76a8a43fd21a1227bfac5e0004696bc
Kin Yuunan <yuunan.kin@gmail.com>
2022-03-20 09:18:25 +0900

    add more cache

--- 66.buddy-pairs.dart
+++ 66.buddy-pairs.dart
@@ -1,13 +1,26 @@
 // https://www.codewars.com/kata/59ccf051dcc4050f7800008f/train/dart
 
 Map<int, int> sumOfDividersCache = {};
+Map<int, List<int>?> buddyCache = {};
 
 List<int>? buddy(int start, int limit) {
+  print('$start-$limit');
   for (int i = start; i <= limit; i++) {
+    if (buddyCache.containsKey(i)) {
+      if (buddyCache[i] == null) continue;
+      return buddyCache[i];
+    }
     int sum = sumOfDividers(i);
-    if (sum - 1 < i) continue;
+    if (sum - 1 < i) {
+      buddyCache[i] = null;
+      continue;
+    }
     int sumsum = sumOfDividers(sum - 1);
-    if (sumsum - 1 == i) return [i, sum - 1];
+    if (sumsum - 1 == i) {
+      buddyCache[i] = [i, sum - 1];
+      return [i, sum - 1];
+    }
+    buddyCache[i] = null;
   }
   return null;
 }
@@ -23,9 +36,14 @@ int sumOfDividers(int n) {
 }
 
 main() {
-  print(buddy(10, 50));
-  print(buddy(200, 1000));
-  print(buddy(57345, 90061));
-  print(buddy(57345, 90061));
+  // print(buddy(10, 50));
+  // print(buddy(200, 1000));
+  // print(buddy(57345, 90061));
+  // print(buddy(57345, 90061));
+  // print(buddy(57345, 90061));
+  // print(buddy(2177, 4357));
+  print(buddy(1071625, 1103735));
   print(sumOfDividersCache.keys.length);
+  print(buddyCache.keys.length);
+  // print(buddyCache[62744]);
 }
