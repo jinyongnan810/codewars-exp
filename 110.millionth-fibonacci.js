7aa774a1dd6b7c2e09c70237d9cd2ffef76a2427
Kin Yuunan <yuunan.kin@gmail.com>
2022-09-08 07:51:41 +0900

    day144(timeout)

--- 110.millionth-fibonacci.js
+++ 110.millionth-fibonacci.js
@@ -1,10 +1,26 @@
 // https://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/javascript
-const cachePlus = [0n, 1n, 1n];
-const cacheMinus = [0n, -1n];
+const cachePlus = [0n, 1n];
+const cacheMinus = [0n, 1n];
 let min = -1;
-let max = 2;
+let max = 1;
 
 const fibPlus = (n) => {
+  if (n <= max) {
+    return cachePlus[n];
+  }
+  let a = cachePlus[max - 1];
+  let b = cachePlus[max];
+  for (let i = max + 1; i <= n; i++) {
+    const fib = a + b;
+    // cachePlus.push(fib);
+    a = b;
+    b = fib;
+  }
+  // max = n;
+  return b;
+};
+
+const fibPlus_recursion = (n) => {
   if (n <= max) {
     return cachePlus[n];
   }
@@ -15,16 +31,23 @@ const fibPlus = (n) => {
 };
 
 const fibMinus = (n) => {
+  const isMinus = n % 2 == 0 ? -1n : 1n;
+  const fib = fibPlus(n * -1) * isMinus;
+  return fib;
+};
+
+const fibMinus_recursion = (n) => {
   if (n >= min) {
-    return cachePlus[n * -1];
+    return cacheMinus[n * -1];
   }
   const fib = fibMinus(n + 2) - fibMinus(n + 1);
   min = n;
-  cachePlus[n * -1] = fib;
+  cacheMinus[n * -1] = fib;
   return fib;
 };
 
 function fib(n) {
+  console.log(n);
   if (n == 0) return 0n;
   if (n > 0) {
     return fibPlus(n);
@@ -38,6 +61,7 @@ function main() {
   console.log(cachePlus);
   for (let i = 0; i >= -10; i--) console.log(`fib ${i}: ${fib(i)}`);
   console.log(cacheMinus);
+  // console.log(fib(1043713));
 }
 
 main();
