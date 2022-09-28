ea935155a55bf46843f4889b6fa68aa109f4ec7d
Kin Yuunan <yuunan.kin@gmail.com>
2022-09-29 07:30:35 +0900

    day157(step.2 in process)

--- 120.biggetest-triangle-in-sphere.ts
+++ 120.biggetest-triangle-in-sphere.ts
@@ -13,6 +13,8 @@ function biggestTriangInt(
   const possibilities =
     (insideCount * (insideCount - 1) * (insideCount - 2)) / 6;
   console.log(`possibilities:${possibilities}`);
+  const possibleTriangles = pickItems(3, pointsInside);
+  console.log(JSON.stringify(possibleTriangles));
   return [[]];
 }
 
@@ -32,23 +34,56 @@ const insideTheSphere = (
   return false;
 };
 
-const points_list = [
+const pickItems = (k: number, list: number[][]): number[][][] => {
+  console.log(`(start)k:${k}, list:${JSON.stringify(list)}`);
+  if (k == list.length) return [list];
+  if (k == 1) return [list];
+  let res: number[][][] = [];
+  list.forEach((item, i) => {
+    const restOfList = list.slice(i + 1);
+    const pickKMinus1FromRest = pickItems(k - 1, restOfList);
+    console.log(
+      `  item:${item}, i:${i}, pickKMinus1FromRest:${JSON.stringify(
+        pickKMinus1FromRest
+      )}`
+    );
+    pickKMinus1FromRest.forEach(
+      (picked) => picked.length == k - 1 && res.push([item, ...picked])
+    );
+  });
+  console.log(
+    `(end)k:${k}, list:${JSON.stringify(list)}, res:${JSON.stringify(res)}`
+  );
+  return res;
+};
+
+const pointsList = [
   [1, 2, -4],
   [-3, 2, 4],
   [7, 8, -4],
   [2, 3, 5],
   [-2, -1, 1],
-  [3, 2, 6],
-  [1, 4, 0],
-  [-4, -5, -6],
-  [4, 5, 6],
-  [-2, -3, -5],
-  [-1, -2, 4],
-  [-3, -2, -6],
-  [-1, -4, 0],
-  [2, 1, -1],
 ];
-const sphere_center = [0, 0, 0];
+const center = [1, 2, -2];
 const radius = 8;
 
-biggestTriangInt(points_list, sphere_center, radius);
+// const pointsList = [
+//   [1, 2, -4],
+//   [-3, 2, 4],
+//   [7, 8, -4],
+//   [2, 3, 5],
+//   [-2, -1, 1],
+//   [3, 2, 6],
+//   [1, 4, 0],
+//   [-4, -5, -6],
+//   [4, 5, 6],
+//   [-2, -3, -5],
+//   [-1, -2, 4],
+//   [-3, -2, -6],
+//   [-1, -4, 0],
+//   [2, 1, -1],
+// ];
+// const center = [0, 0, 0];
+// const radius = 8;
+
+biggestTriangInt(pointsList, center, radius);
