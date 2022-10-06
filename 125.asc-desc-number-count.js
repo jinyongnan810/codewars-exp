5bb2ae34256cb14e98d06c0fb78545ef6a209b58
Kin Yuunan <yuunan.kin@gmail.com>
2022-10-07 08:03:42 +0900

    day165

--- 125.asc-desc-number-count.js
+++ 125.asc-desc-number-count.js
@@ -1,4 +1,6 @@
 // https://www.codewars.com/kata/55b195a69a6cc409ba000053/train/javascript
 function totalIncDec(x) {
-  //your code here
+  if (x == 0) return 1;
+  if (x == 1) return 10;
+  if (x == 2) return 100;
 }
