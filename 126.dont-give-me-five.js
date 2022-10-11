703165259681bd7bb5497dde3a435f79ef788cd1
Kin Yuunan <yuunan.kin@gmail.com>
2022-10-12 08:03:40 +0900

    day170(abort)

--- 126.dont-give-me-five.js
+++ 126.dont-give-me-five.js
@@ -1,15 +1,25 @@
 // https://www.codewars.com/kata/621f89cc94d4e3001bb99ef4/train/javascript
 function dontGiveMeFive(start, end) {
-  const total = end - start + 1;
-  if (start / 5 > -10 && end / 5 < 10) {
-    const fives = Math.trunc(end / 5) - Math.trunc(start / 5);
-    const tens = Math.trunc(end / 10) - Math.trunc(start / 10);
-    return total - fives + tens;
+  // const total = end - start + 1;
+  // if (start / 5 > -10 && end / 5 < 10) {
+  //   const fives = Math.trunc(end / 5) - Math.trunc(start / 5);
+  //   const tens = Math.trunc(end / 10) - Math.trunc(start / 10);
+  //   return total - fives + tens;
+  // }
+  // return 0;
+  let res = 0;
+  for (let i = start; i <= end; i++) {
+    if (!/5/.test(i)) {
+      res++;
+    }
   }
-  return 0;
+  return res;
 }
 
 console.log(dontGiveMeFive(-17, 9));
 console.log(dontGiveMeFive(1, 9));
 console.log(dontGiveMeFive(4, 17));
 console.log(dontGiveMeFive(-17, -4));
+
+// solutions
+// https://www.codewars.com/kata/621f89cc94d4e3001bb99ef4/solutions
