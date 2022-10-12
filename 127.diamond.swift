b5a5d10799106846f93f1f13183b7a1e86201ff1
Kin Yuunan <yuunan.kin@gmail.com>
2022-10-13 07:43:44 +0900

    day171

--- 127.diamond.swift
+++ 127.diamond.swift
@@ -1,4 +1,26 @@
 // https://www.codewars.com/kata/5503013e34137eeeaa001648/train/swift
-func diamond(_ size: Int) -> String {
-  return ""
+func diamond(_ size: Int) -> String? {
+  if size.isMultiple(of: 2) {
+    return nil
+  }
+  if size<1 {
+    return nil
+  }
+  var res = [String]()
+  let half = (size+1)/2
+  print("size:\(size)")
+  for level in 1...size {
+    if level<half {
+      res.append(String(repeating: " ", count: half-level)+String(repeating: "*", count: (level-1)*2+1))
+    } else if level == half {
+      res.append(String(repeating: "*", count: size))
+    } else {
+      res.append(String(repeating: " ", count: level-half)+String(repeating: "*", count: (size-level)*2+1))
+    }
+  }
+
+  return res.joined(separator: "\n") + "\n"
 }
+
+let res = diamond(5)
+print(res ?? "nil")
