13e1337a609ba119c0565932bcc0a61a115fbce7
Kin Yuunan <yuunan.kin@gmail.com>
2022-10-18 07:25:45 +0900

    day175

--- 130.screen-lock-pattern.dart
+++ 130.screen-lock-pattern.dart
@@ -2,6 +2,39 @@
 class Lock {
   List<Dot> dots;
   Lock({required this.dots});
+  void markUsed(String dot) {
+    dots.firstWhere((d) => d.displayText == dot).used = true;
+  }
+
+  Dot getDot(int row, int column) {
+    return dots.firstWhere((dot) => dot.row == row && dot.column == column);
+  }
+
+  List<Dot> nextMove(Dot current) {
+    List<Dot> res = [];
+    dots.forEach((dot) {
+      if (dot.used) {
+        return;
+      }
+      if ((dot.row - current.row).abs() <= 1 &&
+          (dot.column - current.column).abs() <= 1) {
+        res.add(dot);
+      } else if (dot.row == current.row &&
+          (dot.column - current.column).abs() == 2) {
+        final between = getDot(dot.row, 1);
+        if (between.used) {
+          res.add(dot);
+        }
+      } else if (dot.column == current.column &&
+          (dot.row - current.row).abs() == 2) {
+        final between = getDot(1, dot.column);
+        if (between.used) {
+          res.add(dot);
+        }
+      }
+    });
+    return res;
+  }
 }
 
 class Dot {
@@ -17,7 +50,7 @@ class Dot {
   });
 }
 
-int countPatternsFrom(String f, int l) {
+int countPatternsFrom(String start, int points) {
   final dots = [
     Dot(displayText: "A", used: false, row: 0, column: 0),
     Dot(displayText: "B", used: false, row: 0, column: 1),
@@ -30,5 +63,23 @@ int countPatternsFrom(String f, int l) {
     Dot(displayText: "I", used: false, row: 2, column: 2),
   ];
   final lock = Lock(dots: dots);
+
+  return countPatternsFromSub(start, points, lock);
+}
+
+int countPatternsFromSub(String start, int points, Lock currentLock) {
+  if (points == 0 || points > 9) return 0;
+  if (points == 1) return 1;
+  // clone lock
+  final dots = currentLock.dots
+      .map((e) => Dot(
+          displayText: e.displayText,
+          used: e.used,
+          row: e.row,
+          column: e.column))
+      .toList();
+  final lock = Lock(dots: dots);
+  lock.markUsed(start);
+
   return 0;
 }
