82e4d839c45e1e52a6d835a0f6e6055b93e3aa13
Kin Yuunan <yuunan.kin@gmail.com>
2022-10-22 09:28:27 +0900

    day179

--- 132.help-the-book-seller.dart
+++ 132.help-the-book-seller.dart
@@ -1,5 +1,24 @@
 // https://www.codewars.com/kata/54dc6f5a224c26032800005c/train/dart
-String stockSummary(List<String> lstOfArt, lstOf1stLetter) {
-    // your code
-    return "";
+class StocksInCategory {
+  String category;
+  int stock;
+  StocksInCategory(this.category, this.stock);
+}
+
+String stockSummary(List<String> stock, List<String> categoriesRequired) {
+  if (stock.isEmpty || categoriesRequired.isEmpty) {
+    return '';
+  }
+  List<StocksInCategory> res = [];
+  categoriesRequired.forEach((category) {
+    final count = stock.fold<int>(0, (previousValue, current) {
+      if (current.startsWith(category)) {
+        final currentCount = int.parse(current.split(' ')[1]);
+        return previousValue + currentCount;
+      }
+      return previousValue;
+    });
+    res.add(StocksInCategory(category, count));
+  });
+  return res.map((r) => '(${r.category} : ${r.stock})').join(' - ');
 }
