6ad57469529b2f37463863e5d59e3d760868eb0f
Kin Yuunan <yuunan.kin@gmail.com>
2022-10-23 10:01:21 +0900

    day180

--- 133.become-immortal.dart
+++ 133.become-immortal.dart
@@ -1,7 +1,31 @@
 // https://www.codewars.com/kata/59568be9cc15b57637000054/train/dart
+import 'dart:math';
+
 /** set true to enable debug */
 bool debug = false;
 
-int elderAge(int n, int m, int k, int newp) {
-  return 0 // do it!
+int elderAge(int n, int m, int loss, int maxTime) {
+  return 0; // do it!
+}
+
+// int xor(int a, int b) {
+//   String binaryA = a.toRadixString(2);
+//   String binaryB = b.toRadixString(2);
+//   final l = binaryA.length > binaryB.length ? binaryA.length : binaryB.length;
+//   binaryA = binaryA.padLeft(l, '0');
+//   binaryB = binaryB.padLeft(l, '0');
+//   List<int> xorList = [];
+//   for (int i = 0; i < l; i++) {
+//     if (binaryA[i] == binaryB[i]) {
+//       xorList.add(0);
+//     } else {
+//       xorList.add(1);
+//     }
+//   }
+//   final xorStr = xorList.join('');
+//   return int.parse(xorStr, radix: 2);
+// }
+
+void main() {
+  print(3 ^ 5);
 }
