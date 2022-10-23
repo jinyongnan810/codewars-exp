4877ce2c51c055fc14ed556a47447e096af409b5
Kin Yuunan <yuunan.kin@gmail.com>
2022-10-24 07:09:42 +0900

    day181(works but need great performance improvement)

--- 133.become-immortal.dart
+++ 133.become-immortal.dart
@@ -1,11 +1,34 @@
 // https://www.codewars.com/kata/59568be9cc15b57637000054/train/dart
-import 'dart:math';
 
 /** set true to enable debug */
 bool debug = false;
 
 int elderAge(int n, int m, int loss, int maxTime) {
-  return 0; // do it!
+  final rect = getRectangle(n, m, loss);
+  final total = getTotal(rect);
+  return total % maxTime;
+}
+
+List<List<int>> getRectangle(int n, int m, int loss) {
+  List<List<int>> res = [];
+  for (int i = 0; i < m; i++) {
+    List<int> row = [];
+    for (int j = 0; j < n; j++) {
+      final time = (i ^ j) - loss;
+      row.add(time < 0 ? 0 : time);
+    }
+    res.add(row);
+  }
+  return res;
+}
+
+int getTotal(List<List<int>> rect) {
+  int res = 0;
+  rect.forEach((row) {
+    res +=
+        row.fold<int>(0, (previousValue, element) => previousValue + element);
+  });
+  return res;
 }
 
 // int xor(int a, int b) {
@@ -27,5 +50,14 @@ int elderAge(int n, int m, int loss, int maxTime) {
 // }
 
 void main() {
-  print(3 ^ 5);
+  print(3 ^ 5); // ^ means xor
+  // print(getRectangle(8, 8, 0));
+  print(elderAge(8, 5, 1, 100));
+  print(elderAge(8, 8, 0, 100007));
+  print(elderAge(25, 31, 0, 100007));
+  print(elderAge(5, 45, 3, 1000007));
+  print(elderAge(31, 39, 7, 2345));
+  // print(elderAge(545, 435, 342, 1000007));
+  // // You need to run this test very quickly before attempting the actual tests :)
+  // print(elderAge(28827050410, 35165045587, 7109602, 13719506));
 }
