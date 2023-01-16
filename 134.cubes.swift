fc9a307a91476f80e3f339465ff4109535377eb1
Kin Yuunan <yuunan.kin@gmail.com>
2023-01-17 08:46:37 +0900

    day183

--- 134.cubes.swift
+++ 134.cubes.swift
@@ -1,6 +1,7 @@
 // https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/swift
+// solution 1 by caching each cube's volume
 var cached = [Int]()
-func findNb(_ number: Int) -> Int {
+func findNb_1(_ number: Int) -> Int {
     var index = 1
     var total = 0
     while true {
@@ -25,5 +26,39 @@ func calculateVolume(_ num: Int) -> Int {
     cached.append(volume)
     return volume
 }
-// TODO: cache further
+// solution 2 by caching n cubes total volume
+var totalCached: [Int] = [0]
+func findNb(_ number: Int) -> Int {
+    var index = 1
+    while true {
+        let total = calculateTotalVolume(index)
+        if total == number {
+            return index
+        } else if total > number {
+            return -1
+        }
+        index += 1
+    }
+}
+
+func calculateTotalVolume(_ num: Int) -> Int {
+    let totalCachedLength = totalCached.count
+    if totalCachedLength > num {
+        let cache = totalCached[num]
+        return cache
+    }
+    for cur in totalCachedLength...num {
+        // print("cur:\(cur)")
+        let pre = totalCached.last!
+        let volume = cur*cur*cur
+        totalCached.append(volume + pre)
+        // print("totalCached:\(totalCached)")
+    }
+    // print("calculateTotalVolume \(num) = \(totalCached.last!)")
+    return totalCached.last!
+}
 // TODO: https://en.wikipedia.org/wiki/Squared_triangular_number
+
+let total =  calculateTotalVolume(2022)
+print("total: \(total)")
+print("n: \(findNb(4183059834009))")
